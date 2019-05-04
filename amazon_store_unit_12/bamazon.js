a0049074963ba196567e5a22f40e734ca850d9f4
Andrew Robinson <arobinson1289@gmail.com>
2019-05-04 16:29:01 -0400

    some changes

---
+++ amazon_store_unit_12/bamazon.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+const db = mysql.createConnection({
+    host:"localhost",
+    port: 3306,
+    user: "root",
+    password:"root",
+    database:"bamazon_DB"
+})
+
+db.connect(function(err){
+  if(err){
+    console.log(`error connecting ${err.stack}`);
+  }else {
+      Getdb()
+    };
+  
+})
+
+function Getdb (){
+    var sqlQuery = "SELECT * FROM products;"
+
+    db.query(sqlQuery, function(err,res){
+        if(err) throw err;
+      
+        console.log(sqlQuery);
+
+    })
+}
--- amazon_store_unit_12/schema.sql
+++ amazon_store_unit_12/schema.sql
@@ -1,2 +1,13 @@
-  INSERT INTO bid_information (product_name,starting_bid,highest_bid)
-    VALUES("bicycle",100,200);
+  INSERT INTO products (item_name,price,stock)
+    VALUES (
+    ("bicycle",100,30),
+	  ("basketball",20,50),
+    ("baseball glove",25,15),
+    ("denim jeans",50,50),
+    ("rain jacket",40,30),
+    ("fishing pole",30,10),
+    ("lamp",40,12),
+    ("sunglasses",75,20),
+    ("running shoes",90,5),
+    ("picture frame",30,25)
+    )
